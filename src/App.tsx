30ccba33160a7f6f2313fce849aae10425526ff1
Samuel F <samuel.farkas98@gmail.com>
2022-04-23 15:14:42 +0200

    setup router and prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
-import {Flex} from "@chakra-ui/react";
+import { Flex } from "@chakra-ui/react"
+import { Routes, Route } from "react-router-dom"
+import { Dashboard } from "./Dashboard"
+import { Intro } from "./intro"
 
 function App() {
   return (
-    <Flex width="100%" justify='center'>
-      depresia:)
+    <Flex width="100%" justify="center">
+      <Routes>
+        <Route path="/" element={<Intro />} />
+        <Route path="dashboard" element={<Dashboard />} />
+      </Routes>
     </Flex>
   )
 }
